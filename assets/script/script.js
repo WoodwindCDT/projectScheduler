35542fac7abf33e9a7f775aa3cc3844d744177b1
Ender <woodwind.turbeville@gmail.com>
2020-08-30 01:21:35 -0500

    Added detialed comments, detailing, and minor corrections

--- assets/script/script.js
+++ assets/script/script.js
@@ -6,8 +6,10 @@ $(document).ready(function() {
     let pageDate = $('#currentDay');
     $(pageDate).text(now);
 
+    // LocalStorage Initiation
     var scheduler = JSON.parse(localStorage.getItem("scheduler")) || startScheduler();
 
+    // Loop to create Table sections with corresponding classes
     for (var hour in scheduler) {
         var tr = $("<tr>")
         .addClass("row data-row")
@@ -17,8 +19,10 @@ $(document).ready(function() {
         var textEvent = $("<td>")
         .addClass("textDisplay col-9")
         
+        // Current Hour
         var currHr;
 
+        // To set textarea background color corresponding to the current Hour
         if (moment(hour, "h a").isSame(moment(), "hour")) {
             currHr = "present";
         }
@@ -32,9 +36,14 @@ $(document).ready(function() {
         var divText = $("<textarea>")
         .addClass("description")
         .addClass(currHr)
+        // data-time creates a attribute to pull data from
+        // Create a data-time = "hour" EX: data-time = "4 pm"
         .attr('data-time', hour)
+        // enabling localStorage value to equal [hour] 
+        // EX : 4pm created via '.attr('data-time', hour)'
         .val(scheduler[hour]);
 
+        // Appending Table element section
         divText.appendTo(textEvent);
 
         var divSave = $("<td>")
@@ -50,15 +59,21 @@ $(document).ready(function() {
         tr.appendTo($("#rowStart"));
     };
 
+    // Function to create Array and Save inner textarea content to a string
+    // Text area content is being pulled by parent .find(.description)
     function startScheduler() {
         var tempArr = {};
 
+        // Looping to create array value content to prepare
+        // for saving tasks to key:Scheduler with EX: 9am: "" 
+        // "" = textcontent and 9am is pulled from moment in time block
         for (var i = 9; i < 18; i++) {
             tempArr[moment(i, "H").format("h a")] = "";
         }
         return tempArr;
     }
 
+    // Save button Input
     $(".saveBtn").on("click", function() {
         var hours = $(this)
         .parent()
@@ -71,9 +86,12 @@ $(document).ready(function() {
 
         scheduler[hours] = text;
 
+        // Setting content within local storage to the scheduler array
         localStorage.setItem("scheduler", JSON.stringify(scheduler));
     });
 
+    // Clear button to reset all saved key / input value
+    // and clearing/re-setting text content in textarea
     $(".clearBtn").on("click", function() {
         window.localStorage.clear();
         $("textarea").val("");
